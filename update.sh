f05509a1c6d2291dd3b9c3c7691f3b95bd320110
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-01-17 12:38:39 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,15 +6,7 @@ echo "##################################################"
 echo "         Run after installing Kali Linux          "                                      	
 echo " This will reboot the Machine after Installation  "
 echo "##################################################"
-sleep 2
-
-PS3='Please enter your choice: '
-options=("Update and Upgrade Packages" "Show Commands" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Update and Upgrade Packages")
-            echo "Installing Updates and Upgrades To Your Machine"
+sleep 3
 clear
 apt-get update
 apt-get upgrade -y
@@ -24,6 +16,14 @@ apt-get -y install ntp
 cp -f desktop-grub.png /usr/share/images/desktop-base/
 cp -f grub-4x3.png /boot/grub/themes/kali/
 cp -f grub-16x9.png /boot/grub/themes/kali/
+apt-get install -y linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
+dpkg -i dkms_3.0.12-1_all.deb
+dpkg -i b43-fwcutter_019-4_amd64.deb
+dpkg -i firmware-b43-installer_019-4_all.deb
+dpkg -i firmware-b43legacy-installer_019-4_all.deb
+dpkg -i broadcom-sta-dkms_6.30.223.271-23_all.deb
+modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
+modprobe wl
 apt-get -y  install grub-customizer
 cd ..
 git clone https://github.com/aircrack-ng/rtl8812au.git
@@ -31,21 +31,3 @@ cd rtl8812au
 make 
 make install
 reboot
-        ;;
-        "Show Commands")
-            echo "apt-get update"
-            echo "apt-get upgrade"
-            echo "apt-get dist-upgrade"
-            echo "apt-get clean"
-            echo "apt-get -y install ntp"
-            echo "chmod +x BCM4331.sh"
-            
-            
-;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-done
- 
